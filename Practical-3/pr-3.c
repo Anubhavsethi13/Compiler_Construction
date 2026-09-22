391afc1a3fb42e45eba5b2f33769b9963d2b43ca
Anubhav Sethi <aanubhavsethi2405@gmail.com>
2026-09-22 11:42:16 +0530

    Add Practical-3

---
+++ Practical-3/pr-3.c
@@ -0,0 +1,16 @@
+%{
+#include<stdio.h>
+int count = 0;
+%}
+%%
+[Aa][a-zA-Z]*	{ Count++; }
+.|\n		{ /* ignore other characters */ }
+%%
+int main() {
+	yylex();
+	printf("Number of Words Starting with 'A' or 'a': %d\n", count);
+	return 0;
+}
+int yywrap() {
+    return 1;
+}
